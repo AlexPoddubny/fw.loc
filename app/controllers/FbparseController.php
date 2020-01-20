ae17a15559f55f7351f5b1e08131b6284faa1da1
AlexPoddubny <usatij.alex@gmail.com>
2020-01-20 21:48:58 +0200

    Facebook Comment parser

--- app/controllers/FbparseController.php
+++ app/controllers/FbparseController.php
@@ -22,6 +22,7 @@
 		protected function process($page)
 		{
 			$str = '';
+			$name = '';
 			$label1 = 'UFI2Comment/root_depth_0';
 			$label2 = 'UFI2Comment/body';
 			$label3 = '_72vr">';
@@ -29,7 +30,7 @@
 				$page = $this->after($page, $label1);
 				$page = $this->after($page, $label2);
 				$page = $this->after($page, $label3);
-				$str .= $this->between('>', '</a>', $page) . "\n";
+				$str .= $this->delSpaces($this->between('>', "<", $page)) . "\n";
 			}
 			return $str;
 		}
@@ -45,7 +46,18 @@
 		
 		protected function between($after, $before, $haystack)
 		{
-			return $this->before($before, $this->after($after, $haystack));
+			return $this->before($this->after($haystack, $after), $before);
+		}
+		
+		protected function str_replace_once($search, $replace, $text)
+		{
+			$pos = strpos($text, $search);
+			return $pos!==false ? substr_replace($text, $replace, $pos, strlen($search)) : $text;
+		}
+		
+		protected function delSpaces($str)
+		{
+			return str_replace("\t", '', $this->str_replace_once("\t", '&nbsp', str_replace("\n", '', str_replace("\r", '', $str))));
 		}
 		
 	}
